10f00bfde2ef9208ccbc3c5c92ee09cfe090dc14
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-09 00:35:28 +0600

    Update

--- apis/project-controllers-api.ts
+++ apis/project-controllers-api.ts
@@ -225,6 +225,54 @@ export const ProjectControllersApiAxiosParamCreator = function (configuration?: 
         /**
          * 
          * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getTasksByStatus: async (id: string, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'id' is not null or undefined
+            if (id === null || id === undefined) {
+                throw new RequiredError('id','Required parameter id was null or undefined when calling getTasksByStatus.');
+            }
+            const localVarPath = `/api/project/{id}/task/status`
+                .replace(`{${"id"}}`, encodeURIComponent(String(id)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, 'https://example.com');
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+            const localVarRequestOptions :AxiosRequestConfig = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication Bearer required
+            // http bearer authentication required
+            if (configuration && configuration.accessToken) {
+                const accessToken = typeof configuration.accessToken === 'function'
+                    ? await configuration.accessToken()
+                    : await configuration.accessToken;
+                localVarHeaderParameter["Authorization"] = "Bearer " + accessToken;
+            }
+
+            const query = new URLSearchParams(localVarUrlObj.search);
+            for (const key in localVarQueryParameter) {
+                query.set(key, localVarQueryParameter[key]);
+            }
+            for (const key in options.params) {
+                query.set(key, options.params[key]);
+            }
+            localVarUrlObj.search = (new URLSearchParams(query)).toString();
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
+         * @param {string} id 
          * @param {UpdateProjectSchema} [body] 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -340,6 +388,19 @@ export const ProjectControllersApiFp = function(configuration?: Configuration) {
         /**
          * 
          * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async getTasksByStatus(id: string, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => Promise<AxiosResponse<Array<string>>>> {
+            const localVarAxiosArgs = await ProjectControllersApiAxiosParamCreator(configuration).getTasksByStatus(id, options);
+            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+                const axiosRequestArgs :AxiosRequestConfig = {...localVarAxiosArgs.options, url: basePath + localVarAxiosArgs.url};
+                return axios.request(axiosRequestArgs);
+            };
+        },
+        /**
+         * 
+         * @param {string} id 
          * @param {UpdateProjectSchema} [body] 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -396,6 +457,15 @@ export const ProjectControllersApiFactory = function (configuration?: Configurat
          */
         async getProjectsList(name?: string, options?: AxiosRequestConfig): Promise<AxiosResponse<Array<Project>>> {
             return ProjectControllersApiFp(configuration).getProjectsList(name, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async getTasksByStatus(id: string, options?: AxiosRequestConfig): Promise<AxiosResponse<Array<string>>> {
+            return ProjectControllersApiFp(configuration).getTasksByStatus(id, options).then((request) => request(axios, basePath));
         },
         /**
          * 
@@ -457,6 +527,16 @@ export class ProjectControllersApi extends BaseAPI {
      */
     public async getProjectsList(name?: string, options?: AxiosRequestConfig) : Promise<AxiosResponse<Array<Project>>> {
         return ProjectControllersApiFp(this.configuration).getProjectsList(name, options).then((request) => request(this.axios, this.basePath));
+    }
+    /**
+     * 
+     * @param {string} id 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ProjectControllersApi
+     */
+    public async getTasksByStatus(id: string, options?: AxiosRequestConfig) : Promise<AxiosResponse<Array<string>>> {
+        return ProjectControllersApiFp(this.configuration).getTasksByStatus(id, options).then((request) => request(this.axios, this.basePath));
     }
     /**
      * 
